e925a8fed347b3a12da4812be5136d868ffe49e2
Shayan <shayanbahal@gmail.com>
2016-11-21 13:21:57 -0500

    dynamic tx_fee

--- pycoin/convention/tx_fee.py
+++ pycoin/convention/tx_fee.py
@@ -1,10 +1,13 @@
 
 import io
+from urllib2 import Request, urlopen, URLError
+import json
+
 
 TX_FEE_PER_THOUSAND_BYTES = 10000
 
 
-def recommended_fee_for_tx(tx):
+def recommended_fee_for_tx(tx, recommanded_fee = True, default_fpk = "low_fee_per_kb", cap = 100000):
     """
     Return the recommended transaction fee in satoshis.
     This is a grossly simplified version of this function.
@@ -12,9 +15,25 @@ def recommended_fee_for_tx(tx):
       - whether the transaction contains "dust"
       - whether any outputs are less than 0.001
       - update for bitcoind v0.90 new fee schedule
+
+    default_fpk can be low_fee_per_kb, medium_fee_per_kb, high_fee_per_kb
+
     """
     s = io.BytesIO()
     tx.stream(s)
     tx_byte_count = len(s.getvalue())
+    if recommanded_fee:
+        try:
+            request = Request('http://api.blockcypher.com/v1/btc/main')
+            response = urlopen(request)
+            RecomFeeResp = json.loads(response.read())
+            TX_FEE_PER_THOUSAND_BYTES = RecomFeeResp.get(default_fpk)
+        except URLError, e:
+            TX_FEE_PER_THOUSAND_BYTES = 10000
+
     tx_fee = TX_FEE_PER_THOUSAND_BYTES * ((999+tx_byte_count)//1000)
+
+    if tx_fee > cap:
+        return cap
+
     return tx_fee
